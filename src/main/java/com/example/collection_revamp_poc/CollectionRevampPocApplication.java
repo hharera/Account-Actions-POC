4ef6e90a85eaf00e22bd3894423593ebf22eea7e
Hassan Harera <hassan.shaabanhassan@vodafone.com.eg>
2023-09-06 17:26:21 +0300

    initialize

---
+++ src/main/java/com/example/collection_revamp_poc/CollectionRevampPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.collection_revamp_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionRevampPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionRevampPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/collection_revamp_poc/CollectionRevampPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collection_revamp_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionRevampPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
